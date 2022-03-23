af51b12856e4aeb79bcf633eea95c6f4f664779c
Keath Milligan <keath@keathmilligan.net>
2022-03-22 21:17:47 -0500

    Add project files.

---
+++ svgview/SVGView.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace svgview
+{
+    partial class SVGView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.svgImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.svgImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // svgImage
+            // 
+            this.svgImage.BackColor = System.Drawing.Color.Black;
+            this.svgImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.svgImage.Location = new System.Drawing.Point(0, 0);
+            this.svgImage.Name = "svgImage";
+            this.svgImage.Size = new System.Drawing.Size(800, 450);
+            this.svgImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.svgImage.TabIndex = 0;
+            this.svgImage.TabStop = false;
+            this.svgImage.Click += new System.EventHandler(this.svgImage_Click);
+            this.svgImage.DoubleClick += new System.EventHandler(this.svgImage_DoubleClick);
+            // 
+            // SVGView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.svgImage);
+            this.Name = "SVGView";
+            this.Text = "SVGView";
+            ((System.ComponentModel.ISupportInitialize)(this.svgImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox svgImage;
+    }
+}
---
+++ svgview/SVGView.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Svg;
+
+namespace svgview
+{
+    public partial class SVGView : Form
+    {
+        private string svgFilename;
+
+        public SVGView(string svgFilename)
+        {
+            this.svgFilename = svgFilename;
+            InitializeComponent();
+            RenderSvg();
+        }
+
+        private void RenderSvg()
+        {
+            svgImage.Image?.Dispose();
+            var dir = Directory.GetCurrentDirectory();
+            var svgDoc = SvgDocument.Open<SvgDocument>(svgFilename, null);
+            svgImage.Image = svgDoc.Draw();
+        }
+
+        private void svgImage_DoubleClick(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                this.TopMost = true;
+                this.FormBorderStyle = FormBorderStyle.None;
+                this.WindowState = FormWindowState.Maximized;
+                //this.Bounds = Screen.PrimaryScreen.Bounds;
+            }
+            else
+            {
+                this.TopMost = false;
+                this.WindowState = FormWindowState.Normal;
+                this.FormBorderStyle = FormBorderStyle.Sizable;
+            }
+        }
+
+        private void svgImage_Click(object sender, EventArgs e)
+        {
+            RenderSvg();
+        }
+    }
+}
